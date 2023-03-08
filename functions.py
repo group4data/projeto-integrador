4b69a28aadd7191b64ed40c11ec5127c0ce58d6f
nogueiragabriela <nogueiragabriela93@gmail.com>
2023-03-08 14:18:10 -0300

    refactor: Updating the verification functions

--- functions.py
+++ functions.py
@@ -4,7 +4,7 @@ import os
 
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
 from pyspark.sql import functions as f
-from pyspark.sql.functions import col,isnan,when,count
+from pyspark.sql.functions import col,isnan,when,count, isnull
 
 
 # Transforma varios arquivos csv em um Data Frame 
@@ -23,65 +23,41 @@ def transform_csv_to_df(spark, path, columns):
     if not all(isinstance(col, str) for col in columns):
         raise ValueError("columns deve ser uma lista de strings.")
 
-def validate_clients(spark, path, columns):
-    #path = './Data/Clients'
-    clients = spark.read.csv(path, sep=';', inferSchema=True)
-    
-    clients = clients.toDF(*columns)
+    df_header = None
+    for file in list_paths_csv:
+        with open(file, 'r') as file_csv:
+            leitor = csv.reader(file_csv)
+            first_line = next(leitor)[0].split(';')
 
-    clients = clients.filter(~col('id').contains('id'))
+            if first_line == columns:
+                df_header = spark.read.csv(file, sep=';', header=True, inferSchema=True)
+                list_paths_csv.remove(file)
+                break
 
-    clients = clients.withColumn("data_cadastro", f.to_timestamp("data_cadastro"))
-    clients = clients.withColumn("id",clients.id.cast(IntegerType()))
+    if df_header is None:
+        raise ValueError(f"Nenhum arquivo com a primeira linha igual a {columns} foi encontrado.")
 
-    return clients
-
-
-def verify_empty_clients_data(clients):
-    clients_string_columns = ['nome','email','telefone']
-    count_empty_strings = clients.select([count(when(col(c).contains('None') | \
-                            col(c).contains('NULL') | \
-                            (col(c) == '' ) | \
-                            col(c).isNull() | \
-                            isnan(c), c 
-                           )).alias(c)
-                    for c in clients_string_columns])
-    
-    print("Number of null or empty values in the following columns.")
-    count_empty_strings.show()
-
-    clients_number_columns = ['id','data_cadastro']
-    count_empty_numbers = clients.select([count(when(col(c).isNull(), c)).alias(c)
-                    for c in clients_number_columns])
-    
-    print("Number of null or empty values in the following columns.")
-    count_empty_numbers.show()
+    try:
+        df_no_header = spark.read.csv(list_paths_csv, sep=';', header=False, inferSchema=True)
+        return df_header.unionAll(df_no_header)
+    except Exception as e:
+        raise RuntimeError(f"Ocorreu um erro ao unir os DataFrames: {str(e)}")
 
 
-def verify_clients_schema(clients, columns):
-    expected_columns = columns
-    expected_schema = StructType([
-        StructField("id", IntegerType(), True),
-        StructField("nome", StringType(), True),
-        StructField("email", StringType(), True),
-        StructField("data_cadastro", TimestampType(), True),
-        StructField("telefone", StringType(), True)
-    ])
+def verify_empty_data(df):
 
-    actual_schema = clients.schema
-
-    if actual_schema == expected_schema:
-        print("The schema is correct!")
-    else:
-        actual_columns = [(f.name, f.dataType) for f in actual_schema.fields]
-        missing_columns = set(expected_columns) - set([c[0] for c in actual_columns])
-        unexpected_columns = set([c[0] for c in actual_columns]) - set(expected_columns)
-        incorrect_types = [(c[0], c[1]) for c in actual_columns if c[0] in expected_columns and c[1] != expected_schema[c[0]].dataType]
-        
-        if missing_columns:
-            print("Error: The following columns are missing: {}".format(missing_columns))
-        if unexpected_columns:
-            print("Error: The following columns are unexpected: {}".format(unexpected_columns))
-        if incorrect_types:
-            print("Error: The following columns have incorrect data types: {}".format(incorrect_types))
+    for col_name in df.columns:
+        data_type = df.schema[col_name].dataType
+        if data_type == StringType():
+            count_empty = df.filter((col(col_name) == '') | isnull(col_name) | isnan(col_name) | (col(col_name).isNull())).count()
+            if count_empty != 0:
+                print(f"Column '{col_name}' has {count_empty} empty/null/none/NaN values.")
+        elif data_type == IntegerType():
+            count_null = df.filter(col(col_name).isNull()).count()
+            if count_null != 0:
+                print(f"Column '{col_name}' has {count_null} null values.")
+        elif data_type == TimestampType():
+            count_null = df.filter(col(col_name).isNull()).count()
+            if count_null != 0:
+                print(f"Column '{col_name}' has {count_null} null values.")
 
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.window import Window
-from functions import transform_csv_to_df, validate_clients, verify_empty_clients_data, verify_clients_schema
-
+from functions import transform_csv_to_df, verify_empty_data
 spark = SparkSession.builder \
     .master('local[*]') \
     .appName("Iniciando com Spark") \
     .config('spark.ui.port', '4051') \
     .getOrCreate()
 
-clients = "./Data/Clients"
-transactions_in = "./Data/Transactions-in"
-transactions_out = "./Data/Transactions-out"
+clients = "Data/Clients"
+transactions_in = "Data/Transactions-in"
+transactions_out = "Data/Transactions-out"
 
 columns_clients = ['id', 'nome', 'email', 'data_cadastro', 'telefone']
 columns_transactions = ['id', 'cliente_id', 'valor', 'data']
@@ -20,15 +19,12 @@ try:
     df_clients = transform_csv_to_df(spark, clients, columns_clients)
     df_transactions_in = transform_csv_to_df(spark, transactions_in, columns_transactions)
     df_transactions_out = transform_csv_to_df(spark, transactions_out , columns_transactions)
+    
+    print("Verificando se há dados não informados nas colunas dos DataFrames!")
+    verify_empty_data(df_clients)
+    verify_empty_data(df_transactions_in)
+    verify_empty_data(df_transactions_out)
 
-
-except Exception as e:
-    print(f"Ocorreu o seguinte erro: {e}!")
-
-try:
-    df_clients = validate_clients(spark, clients, columns_clients)
-    verify_empty_clients_data(df_clients)
-    verify_clients_schema(df_clients, columns_clients)
 
 except Exception as e:
     print(f"Ocorreu o seguinte erro: {e}!")
