70448914432c5c5f0f65b3e8181a52e6649b3194
goncavestassia <tassia.ap@outlook.com>
2023-03-12 19:36:01 -0300

    feat: creating views

--- sql_query.sql
+++ sql_query.sql
@@ -30,3 +30,72 @@ ALTER TABLE defrauded_clients
 ADD CONSTRAINT fk_clientes_id
 FOREIGN KEY (cliente_id)
 REFERENCES clientes (id);
+
+CREATE VIEW frauds_by_state AS
+    SELECT COUNT(ft.cliente_id) as quantidade_de_fraudes, c.estado 
+    FROM fraudulent_transactions ft 
+    JOIN clientes c 
+    ON c.id = ft.cliente_id 
+    GROUP BY c.estado;
+
+SELECT * 
+FROM frauds_by_state
+ORDER BY quantidade_de_fraudes_por_estado DESC;
+
+CREATE VIEW sum_of_frauds AS
+    SELECT SUM(valor) as somatorio_valor, tipo_transacao
+    FROM  fraudulent_transactions
+    GROUP BY tipo_transacao;
+
+SELECT * 
+FROM sum_of_frauds
+ORDER BY somatorio_valor DESC;
+
+CREATE VIEW count_of_frauds AS
+    SELECT COUNT(valor) as quantidade_transacoes, tipo_transacao
+    FROM  fraudulent_transactions
+    GROUP BY tipo_transacao;
+
+SELECT * 
+FROM count_of_frauds
+ORDER BY quantidade_transacoes DESC;
+
+CREATE VIEW frauds_by_year AS
+    SELECT YEAR(data_hora) AS Ano, COUNT(*) AS frauds_by_year
+    FROM fraudulent_transactions
+    GROUP BY YEAR(data_hora);
+
+SELECT * 
+FROM frauds_by_year
+ORDER BY Ano DESC;
+
+CREATE VIEW frauds_by_shift_and_year AS
+    SELECT DATEPART(year, data_hora) AS ano, 
+        CASE 
+            WHEN DATEPART(hour, data_hora) BETWEEN 0 AND 5 THEN '0-6 horas'
+            WHEN DATEPART(hour, data_hora) BETWEEN 6 AND 11 THEN '6-12 horas'
+            WHEN DATEPART(hour, data_hora) BETWEEN 12 AND 17 THEN '12-18 horas'
+            WHEN DATEPART(hour, data_hora) BETWEEN 18 AND 23 THEN '18-24 horas'
+        END AS horario_transacao,
+        COUNT(*) AS quantidade_transacoes
+    FROM fraudulent_transactions
+    GROUP BY DATEPART(year, data_hora), 
+            CASE 
+                WHEN DATEPART(hour, data_hora) BETWEEN 0 AND 5 THEN '0-6 horas'
+                WHEN DATEPART(hour, data_hora) BETWEEN 6 AND 11 THEN '6-12 horas'
+                WHEN DATEPART(hour, data_hora) BETWEEN 12 AND 17 THEN '12-18 horas'
+                WHEN DATEPART(hour, data_hora) BETWEEN 18 AND 23 THEN '18-24 horas'
+            END;
+
+SELECT * 
+FROM frauds_by_shift_and_year
+ORDER BY ano;
+
+CREATE VIEW frauds_by_month AS
+    SELECT COUNT(id_transaction) AS quantidade_de_fraudes, MONTH(data_hora) AS mes
+    FROM fraudulent_transactions
+    GROUP BY MONTH(data_hora);
+
+SELECT * 
+FROM frauds_by_month
+ORDER BY quantidade_de_fraudes DESC;
